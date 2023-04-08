c8e84a969f6cfb4445dabe961ab6ca75cbc490ce
Dany Mrushi <mrushidany@gmail.com>
2023-04-08 17:23:16 +0300

    Optmizing fonts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,10 @@
 import '@/styles/globals.css'
+import { Inter, Sarabun, Roboto } from 'next/font/google'
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
