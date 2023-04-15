bd32f94e246103a4f5359c9ab8e9385b345b47c1
Dany Mrushi <mrushidany@gmail.com>
2023-04-15 18:17:16 +0300

    Setting Hero component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
+import Hero from './components/Hero'
+
 export default function Home() {
   return (
     <body className="min-h-screen font-sans bg-event-black_2">
-        <main className="flex max-w-[1440px] min-h-screen mx-auto">
-    klklkl
-       </main>
+      <main className="flex max-w-[1440px] min-h-screen mx-auto">
+        <Hero />
+      </main>
     </body>
   )
 }
