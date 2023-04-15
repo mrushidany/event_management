9ea1174135714df7fef030fa566bee59de17c0c7
Dany Mrushi <mrushidany@gmail.com>
2023-04-15 18:35:18 +0300

    Setting up the navigation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,14 @@
 export default function Home() {
   return (
     <main className="flex w-full h-screen font-sans flex-col bg-event-black_2">
-    <div className="bg-hero w-full h-auto">
-        Hero sect
+    <div className="bg-hero w-full h-auto py-[26px] px-[132px]">
+      <nav className="w-full h-auto flex items-center flex-row justify-between">
+          <div className="flex flex-row">
+             <div className="flex flex-col justify-center items-start p-0 gap-[4px] w-[11px] h[15.5px]">
+                <div className="rounded-[0.2px] rotate-[-90deg]"></div>
+             </div>
+          </div>
+      </nav>
     </div>
   </main>
   )
